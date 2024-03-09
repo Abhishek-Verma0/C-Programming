ecab06d73a514e75f2a87da02ecadc9ecfba941f
Abhishek-Verma0 <neeleshabhishek@gmail.com>
2024-03-09 17:43:21 +0530

    Create check_whether_has_9_factors.c

---
+++ Short Programms/check_whether_has_9_factors.c
@@ -0,0 +1,40 @@
+// user input a integer and we will check all numbers from 1 to n who have exactly 9 factors and print them
+#include<stdio.h>
+
+int factor_9(int n);
+
+int main(){
+int num;
+scanf("%d",&num);
+factor_9(num);
+
+}
+
+int factor_9(int n){
+
+
+
+    int numc=0;
+for(int i=1; i<=n;i++){
+    int count=0;
+    for (int j = 1; j <= i; j++)
+    {
+        if (i%j==0)
+        {
+            count++;
+            
+        }
+
+        
+    }
+    if (count==9)
+    {
+        printf("%d ",i);
+        numc++;
+    }
+
+}
+printf("\n%d ",numc);
+
+
+}
